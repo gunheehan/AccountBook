55719da85d80a2f9094d0076f94d977c468e319a
HanGeonHee <han.gh@salin.co.kr>
2024-10-03 22:31:25 +0900

    Notification Android, IOS 분리

--- Assets/Scripts/SMSReader/SMSManager.cs
+++ Assets/Scripts/SMSReader/SMSManager.cs
@@ -1,11 +1,13 @@
 using System;
 using Unity.Notifications.Android;
+using Unity.Notifications.iOS;
 using UnityEngine;
 
 public class SMSManager : MonoBehaviour
 {
     private void Start()
     {
+        #if UNITY_ANDROID
         var channel = new AndroidNotificationChannel()
         {
             Id = "354",
@@ -14,17 +16,65 @@ public class SMSManager : MonoBehaviour
             Description = "Generic notifications",
         };
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
+        #endif
     }
     
-    public void OnSMSReceive (string message) {
-        var notification = new AndroidNotification();
-        notification.Title = "AccountBank";
-        notification.Text = message;
-        notification.FireTime = DateTime.Now;
+    public void OnSMSReceive (string message) 
+    {
+#if UNITY_ANDROID
+        PushNotificationAndriod(message);
+#elif UNITY_IOS
+        PushNotificationIOS(message);
+#endif
+    }
 
-        notification.ShowTimestamp = true;
-        notification.ShouldAutoCancel = true;
-        
-        AndroidNotificationCenter.SendNotification(notification, "354");
+    private void PushNotificationAndriod(string msg)
+    {
+        try
+        {
+            var notification = new AndroidNotification();
+            notification.Title = "AccountBank";
+            notification.Text = msg;
+            notification.FireTime = DateTime.Now;
+
+            notification.ShowTimestamp = true;
+            notification.ShouldAutoCancel = true;
+
+            AndroidNotificationCenter.SendNotification(notification, "354");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private void PushNotificationIOS(string msg)
+    {
+        try
+        {
+            var timetrigger = new iOSNotificationTimeIntervalTrigger();
+            timetrigger.TimeInterval = new TimeSpan(0, 0, 0, 3);
+            timetrigger.Repeats = false;
+
+            var notification = new iOSNotification()
+            {
+                Identifier = "_notification",
+                Title = "AccountBank",
+                Body = msg,
+                ShowInForeground = false,
+                ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+                CategoryIdentifier = "category_a",
+                ThreadIdentifier = "thread1",
+                Trigger = timetrigger
+            };
+
+            iOSNotificationCenter.ScheduleNotification(notification);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }
